e79afa1329d064610faf13e6bba3065587c47740
Felipe Aguilar <luis.aguilartaisiguez@ucr.ac.cr>
2023-05-21 14:03:53 -0600

    api rest login

---
+++ back-risk-register/Services/ILogin.cs
@@ -0,0 +1,13 @@
+﻿using back_risk_register.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_risk_register.Services
+{
+    public interface ILogin
+    {
+        public Login login([FromBody] Login login);
+
+        public IActionResult getuser(string username);
+
+    }
+}
